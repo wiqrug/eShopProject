a4d0c3bb1d55a8131f854d5f1d3ee529e1cbc15f
KonsBrou <mproumas.kostas@gmail.com>
2024-01-08 11:13:54 +0200

    added authenticationFilter

---
+++ Services/AuthenticationFilter.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AuthenticationFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var authHeader = context.HttpContext.Request.Headers["Authorization"];
+        var token = authHeader.ToString().Split(' ')[1];
+
+        if (token == null)
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        try
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("JwtSettings:key");
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out _);
+
+            // You can access the authenticated user from the principal
+            context.HttpContext.Items["User"] = principal;
+
+            await next();
+        }
+        catch (SecurityTokenException)
+        {
+            context.Result = new ForbidResult();
+        }
+    }
+}
